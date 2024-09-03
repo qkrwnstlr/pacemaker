8a4cf3505e934b029dfaa85d0f81a61304865255
최승준 <csjune99@naver.com>
2024-09-04 08:18:24 +0900

    [#S11P21S004-147] [FE] feat: 회원 등록시 회원 정보 입력 presentation layer 구현 및 결합

---
+++ android/presentation/src/main/java/com/ssafy/presentation/loginUI/join/JoinRegisterViewModel.kt
@@ -0,0 +1,27 @@
+package com.ssafy.presentation.loginUI.join
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class JoinRegisterViewModel : ViewModel() {
+    private val _orderState: MutableStateFlow<Int> = MutableStateFlow(1)
+    val orderState: StateFlow<Int> = _orderState.asStateFlow()
+
+    private val _genderState: MutableStateFlow<String> = MutableStateFlow("")
+    val genderState: StateFlow<String> = _genderState.asStateFlow()
+
+    fun nextOrder() {
+        _orderState.value += 1
+    }
+
+    fun resetOrder() {
+        _orderState.value = 1
+    }
+
+    fun setGender(gender: String) {
+        _genderState.value = gender
+    }
+
+}
