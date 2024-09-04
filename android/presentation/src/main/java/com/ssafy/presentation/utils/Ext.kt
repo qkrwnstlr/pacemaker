ec8134c01cf63de3c9ca34385ced937b0691740b
정다혜 <hyejjang1203@naver.com>
2024-09-04 15:49:57 +0900

    [#S11P21S004-188] [FE] feat: 커스텀 month 달력뷰 구현

--- android/presentation/src/main/java/com/ssafy/presentation/utils/Ext.kt
+++ android/presentation/src/main/java/com/ssafy/presentation/utils/Ext.kt
@@ -1,1 +1,40 @@
 package com.ssafy.presentation.utils
+
+import android.content.Context
+import android.widget.TextView
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.findViewTreeLifecycleOwner
+import java.time.DayOfWeek
+import java.time.Month
+import java.time.format.TextStyle
+import java.util.Locale
+
+
+fun DayOfWeek.displayText(uppercase: Boolean = false, narrow: Boolean = false): String {
+    val style = if (narrow) TextStyle.NARROW else TextStyle.SHORT
+    return getDisplayName(style, Locale.ENGLISH).let { value ->
+        if (uppercase) value.uppercase(Locale.ENGLISH) else value
+    }
+}
+
+fun Month.displayText(short: Boolean = true): String {
+    val style = if (short) TextStyle.SHORT else TextStyle.FULL
+    return getDisplayName(style, Locale.ENGLISH)
+}
+
+internal fun TextView.setTextColorRes(@ColorRes color: Int) =
+    setTextColor(context.getColorCompat(color))
+
+internal fun Context.getColorCompat(@ColorRes color: Int) =
+    ContextCompat.getColor(this, color)
+
+fun Fragment.addStatusBarColorUpdate(@ColorRes colorRes: Int) {
+    view?.findViewTreeLifecycleOwner()?.lifecycle?.addObserver(
+        StatusBarColorLifecycleObserver(
+            requireActivity(),
+            requireContext().getColorCompat(colorRes),
+        ),
+    )
+}
