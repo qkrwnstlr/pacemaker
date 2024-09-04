2e7327818f3751e11e7981fbdab4d8cab254b744
허유정 <yu.jeong5899@gmail.com>
2024-09-04 14:28:39 +0900

    [#S11P21S004-214] [BE] feat: 유저 및 코치 엔티티 수정

--- backend/src/main/java/com/pacemaker/domain/user/entity/User.java
+++ backend/src/main/java/com/pacemaker/domain/user/entity/User.java
@@ -4,12 +4,16 @@ import java.time.LocalDateTime;
 
 import com.pacemaker.domain.coach.entity.Coach;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,22 +26,46 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "coach_id", nullable = false)
 	private Coach coach;
+
+	@Column(nullable = false)
 	private String uid;
+
+	@Column(nullable = false, length = 20)
 	private String username;
+
+	@Column(nullable = false)
 	private Integer year;
-	private String gender;
+
+	@Enumerated(EnumType.STRING)
+	private Gender gender;
+
+	@Column(nullable = false)
 	private Integer height;
+
+	@Column(nullable = false)
 	private Integer weight;
+
+	@Column(nullable = false, columnDefinition = "int default 0")
 	private Integer trainCount;
+
+	@Column(nullable = false, columnDefinition = "int default 0")
 	private Integer trainTime;
+
+	@Column(nullable = false, columnDefinition = "int default 0")
 	private Integer trainDistance;
+
+	@Column(nullable = false, columnDefinition = "datetime default now()")
 	private LocalDateTime createdAt;
+
+	@Column(nullable = false, columnDefinition = "int default 0")
 	private Integer vdot;
 
 	@Builder
-	public User(Coach coach, String uid, String username, Integer year, String gender, Integer height, Integer weight,
+	public User(Coach coach, String uid, String username, Integer year, Gender gender, Integer height, Integer weight,
 		Integer trainCount, Integer trainTime, Integer trainDistance, LocalDateTime createdAt, Integer vdot) {
 		this.coach = coach;
 		this.uid = uid;
