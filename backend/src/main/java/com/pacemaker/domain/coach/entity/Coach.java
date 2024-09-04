bc13d98729ccd13c5ec72f7a8620bda4078f19f6
허유정 <yu.jeong5899@gmail.com>
2024-09-04 14:28:39 +0900

    [#S11P21S004-214] [BE] feat: 유저 및 코치 엔티티 수정

--- backend/src/main/java/com/pacemaker/domain/coach/entity/Coach.java
+++ backend/src/main/java/com/pacemaker/domain/coach/entity/Coach.java
@@ -1,5 +1,6 @@
 package com.pacemaker.domain.coach.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,8 +16,14 @@ public class Coach {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(nullable = false, length = 50)
 	private String name;
+
+	@Column(nullable = false)
 	private String description;
+
+	@Column(name = "image_url")
 	private String imageUrl;
 
 	@Builder
