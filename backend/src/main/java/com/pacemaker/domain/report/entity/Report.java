b5bf2f2606c895ce3dd2410a375fd5ae1fc411c8
김재훈 <kgarry@naver.com>
2024-09-04 16:03:31 +0900

    [#S11P21S004-205] [BE] chore: 레포트 엔티티 설정

---
+++ backend/src/main/java/com/pacemaker/domain/report/entity/Report.java
@@ -0,0 +1,82 @@
+package com.pacemaker.domain.report.entity;
+
+import java.time.LocalDateTime;
+
+import com.pacemaker.domain.user.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Report {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@Column(name = "train_date", columnDefinition = "datetime default now()", nullable = false)
+	private LocalDateTime trainDate;
+
+	@Column(name = "train_distance", nullable = false)
+	private Integer trainDistance;
+
+	@Column(name = "train_time", nullable = false)
+	private Integer trainTime;
+
+	@Column(name = "heart_rate")
+	private Integer heartRate;
+
+	@Column(nullable = false)
+	private Integer pace;
+
+	@Column(nullable = false)
+	private Integer cadence;
+
+	@Column(nullable = false)
+	private Integer kcal;
+
+	@Lob
+	@Column(name = "heart_zone")
+	private String heartZone;
+
+	@Lob
+	@Column(name = "split_data")
+	private String splitData;
+
+	@Column(name = "train_type", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private TrainType trainType;
+
+	@Builder
+	public Report(User user, LocalDateTime trainDate, Integer trainDistance, Integer trainTime, Integer heartRate,
+		Integer pace, Integer cadence, Integer kcal, String heartZone, String splitData, TrainType trainType) {
+			this.user = user;
+			this.trainDate = trainDate;
+			this.trainDistance = trainDistance;
+			this.trainTime = trainTime;
+			this.heartRate = heartRate;
+			this.pace = pace;
+			this.cadence = cadence;
+			this.kcal = kcal;
+			this.heartZone = heartZone;
+			this.splitData = splitData;
+			this.trainType = trainType;
+	}
+}
