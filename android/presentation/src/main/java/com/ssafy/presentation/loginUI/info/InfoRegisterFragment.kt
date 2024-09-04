0d77948984d3bf2017d77b4706b541a8f05c97ce
최승준 <csjune99@naver.com>
2024-09-04 09:22:31 +0900

    [#S11P21S004-150] [FE] feat: info 정보 등록 presentation layer 생성

---
+++ android/presentation/src/main/java/com/ssafy/presentation/loginUI/info/InfoRegisterFragment.kt
@@ -0,0 +1,10 @@
+package com.ssafy.presentation.loginUI.info
+
+import com.ssafy.presentation.core.BaseFragment
+import com.ssafy.presentation.databinding.FragmentInfoRegisterBinding
+
+class InfoRegisterFragment : BaseFragment<FragmentInfoRegisterBinding>(
+    FragmentInfoRegisterBinding::inflate
+) {
+
+}
