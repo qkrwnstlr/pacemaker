43b87303db0408da121be65148be9634142d75a8
박준식 <akfldkskgorn@gmail.com>
2024-09-04 15:13:07 +0900

    [#S11P21S004-179] [FE] feat: google maps api 연결

--- android/presentation/build.gradle.kts
+++ android/presentation/build.gradle.kts
@@ -4,6 +4,16 @@ plugins {
     id("androidx.navigation.safeargs.kotlin")
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt.android)
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+}
+
+secrets {
+    propertiesFileName = "secrets.properties"
+
+    defaultPropertiesFileName = "local.defaults.properties"
+
+    ignoreList.add("keyToIgnore")
+    ignoreList.add("sdk.*")
 }
 
 android {
@@ -34,6 +44,7 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
+        buildConfig = true
         viewBinding = true
     }
 }
@@ -57,4 +68,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.play.services.maps)
 }
--- android/presentation/src/main/java/com/ssafy/presentation/homeUI/HomeFragment.kt
+++ android/presentation/src/main/java/com/ssafy/presentation/homeUI/HomeFragment.kt
@@ -1,8 +1,39 @@
 package com.ssafy.presentation.homeUI
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.ssafy.presentation.R
 import com.ssafy.presentation.databinding.FragmentHomeBinding
 import com.ssafy.presentation.core.BaseFragment
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate),
+  OnMapReadyCallback {
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    val mapFragment = SupportMapFragment.newInstance()
+    getParentFragmentManager()
+      .beginTransaction()
+      .add(R.id.map, mapFragment)
+      .commit()
+    mapFragment.getMapAsync(this)
 
+    return super.onCreateView(inflater, container, savedInstanceState)
+  }
+
+  override fun onMapReady(map: GoogleMap) {
+    val point = LatLng(37.514655, 126.979974)
+    map.addMarker(MarkerOptions().position(point).title("현위치"))
+    map.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 12f))
+  }
 }
