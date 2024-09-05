f3c3a4e0747b34af207bd52a18ceac667e4a95ff
허유정 <yu.jeong5899@gmail.com>
2024-09-05 15:47:21 +0900

    [#S11P21S004-219] [BE] feat: git 충돌 해결

--- backend/src/main/java/com/pacemaker/domain/user/controller/UserController.java
+++ backend/src/main/java/com/pacemaker/domain/user/controller/UserController.java
@@ -2,16 +2,12 @@ package com.pacemaker.domain.user.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pacemaker.domain.user.dto.CheckUidResponse;
 import com.pacemaker.domain.user.dto.UserCreateRequest;
-import com.pacemaker.domain.user.dto.UserRequest;
 import com.pacemaker.domain.user.service.UserService;
 
 import io.swagger.v3.oas.annotations.Operation;
