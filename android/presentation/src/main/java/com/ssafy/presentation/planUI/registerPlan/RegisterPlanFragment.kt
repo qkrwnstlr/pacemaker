1f47faa3eb30494ca0e1a6249b4473c1daed6430
최승준 <csjune99@naver.com>
2024-09-05 13:51:25 +0900

    [#S11P21S004-218] [FE] feat: 목표 등록 화면 presentation layer 구현, 코치 부분은 미구현

--- android/presentation/src/main/java/com/ssafy/presentation/planUI/registerPlan/RegisterPlanFragment.kt
+++ android/presentation/src/main/java/com/ssafy/presentation/planUI/registerPlan/RegisterPlanFragment.kt
@@ -1,10 +1,80 @@
 package com.ssafy.presentation.planUI.registerPlan
 
-import com.ssafy.presentation.databinding.FragmentRegisterPlanBinding
+import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.ssafy.presentation.R
 import com.ssafy.presentation.core.BaseFragment
+import com.ssafy.presentation.databinding.FragmentRegisterPlanBinding
+import kotlinx.coroutines.launch
 
 class RegisterPlanFragment : BaseFragment<FragmentRegisterPlanBinding>(
     FragmentRegisterPlanBinding::inflate
 ) {
+    private val viewModel: RegisterPlanViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListener()
+        initCollect()
+    }
+
+    private fun initListener() = with(binding) {
+        fabBlue.apply {
+            text = "훈련 일정 생성"
+            setOnClickListener { viewModel.nextOrder() }
+        }
+    }
+
+    private fun initCollect() = viewLifecycleOwner.lifecycleScope.launch {
+        viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            viewModel.orderState.collect { order ->
+                when (order) {
+                    DISTANCE -> registerDistance()
+                    TIME -> registerTime()
+                    WEEK -> registerWeek()
+                    else -> register()
+                }
+            }
+        }
+    }
+
+    private fun moveToInfo() {
+
+    }
+
+    private fun register() = with(binding) {
+        //TODO 비어있는 공간 확인 로직 추가!
+        moveToInfo()
+        viewModel.resetOrder()
+    }
+
+    private fun registerDistance() = with(binding) {
+        val slideDown = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_slide_down)
+
+        tvTitle.startAnimation(slideDown)
+        tvOrder.startAnimation(slideDown)
+    }
+
+    private fun registerTime() = with(binding) {
+        tilTime.visibility = View.VISIBLE
+        tvOrder.text = "(2/3)"
+    }
+
+    private fun registerWeek() = with(binding) {
+        tvLikeWeek.visibility = View.VISIBLE
+        clWeek.visibility = View.VISIBLE
+        tvOrder.text = "(3/3)"
+    }
+
+    companion object {
+        const val DISTANCE = 1
+        const val TIME = 2
+        const val WEEK = 3
+    }
 }
--- android/presentation/src/main/java/com/ssafy/presentation/planUI/registerPlan/RegisterPlanViewModel.kt
+++ android/presentation/src/main/java/com/ssafy/presentation/planUI/registerPlan/RegisterPlanViewModel.kt
@@ -1,6 +1,22 @@
 package com.ssafy.presentation.planUI.registerPlan
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class RegisterPlanViewModel : ViewModel() {
+    private val _orderState: MutableStateFlow<Int> = MutableStateFlow(1)
+    val orderState: StateFlow<Int> = _orderState.asStateFlow()
+
+    fun nextOrder() {
+        val prev = _orderState.value
+
+        if (prev > 10) _orderState.value -= 1
+        else _orderState.value += 1
+    }
+
+    fun resetOrder() {
+        _orderState.value = 1
+    }
 }
