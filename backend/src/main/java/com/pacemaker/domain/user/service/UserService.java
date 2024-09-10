f2db3e2a1f59a657562af36a64357d1477ff2162
허유정 <yu.jeong5899@gmail.com>
2024-09-10 12:36:45 +0900

    [#S11P21S004-223] [BE] style: 불필요한 애노테이션 삭제

--- backend/src/main/java/com/pacemaker/domain/user/service/UserService.java
+++ backend/src/main/java/com/pacemaker/domain/user/service/UserService.java
@@ -2,7 +2,6 @@ package com.pacemaker.domain.user.service;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,7 +22,6 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class UserService {
 
-	@Autowired
 	private final UserRepository userRepository;
 
 	@Transactional
