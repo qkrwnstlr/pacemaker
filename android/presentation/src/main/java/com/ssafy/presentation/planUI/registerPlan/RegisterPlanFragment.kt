b851523c351ab100f5c72700c7ef4112637ed677
최승준 <csjune99@naver.com>
2024-09-10 09:09:47 +0900

    [#S11P21S004-218] [FE] feat: 코치와의 채팅을 통한 플랜 생성 presentation layer 구현

--- android/presentation/src/main/java/com/ssafy/presentation/planUI/registerPlan/RegisterPlanFragment.kt
+++ android/presentation/src/main/java/com/ssafy/presentation/planUI/registerPlan/RegisterPlanFragment.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import android.view.View
 import android.view.animation.AnimationUtils
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import com.ssafy.presentation.R
 import com.ssafy.presentation.core.BaseFragment
 import com.ssafy.presentation.databinding.FragmentRegisterPlanBinding
-import kotlinx.coroutines.launch
 
 class RegisterPlanFragment : BaseFragment<FragmentRegisterPlanBinding>(
     FragmentRegisterPlanBinding::inflate
@@ -20,61 +15,18 @@ class RegisterPlanFragment : BaseFragment<FragmentRegisterPlanBinding>(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initListener()
-        initCollect()
+        initView()
     }
 
-    private fun initListener() = with(binding) {
-        fabBlue.apply {
-            text = "훈련 일정 생성"
-            setOnClickListener { viewModel.nextOrder() }
-        }
-    }
-
-    private fun initCollect() = viewLifecycleOwner.lifecycleScope.launch {
-        viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-            viewModel.orderState.collect { order ->
-                when (order) {
-                    DISTANCE -> registerDistance()
-                    TIME -> registerTime()
-                    WEEK -> registerWeek()
-                    else -> register()
-                }
-            }
-        }
-    }
-
-    private fun moveToInfo() {
-
-    }
-
-    private fun register() = with(binding) {
-        //TODO 비어있는 공간 확인 로직 추가!
-        moveToInfo()
-        viewModel.resetOrder()
-    }
-
-    private fun registerDistance() = with(binding) {
-        val slideDown = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_slide_down)
+    private fun initView() = with(binding) {
+        val slideDown = AnimationUtils.loadAnimation(
+            requireContext(),
+            com.ssafy.presentation.R.anim.fade_slide_down
+        )
 
         tvTitle.startAnimation(slideDown)
-        tvOrder.startAnimation(slideDown)
+        tvLikeWeek.startAnimation(slideDown)
+        clWeek.startAnimation(slideDown)
     }
 
-    private fun registerTime() = with(binding) {
-        tilTime.visibility = View.VISIBLE
-        tvOrder.text = "(2/3)"
-    }
-
-    private fun registerWeek() = with(binding) {
-        tvLikeWeek.visibility = View.VISIBLE
-        clWeek.visibility = View.VISIBLE
-        tvOrder.text = "(3/3)"
-    }
-
-    companion object {
-        const val DISTANCE = 1
-        const val TIME = 2
-        const val WEEK = 3
-    }
 }
