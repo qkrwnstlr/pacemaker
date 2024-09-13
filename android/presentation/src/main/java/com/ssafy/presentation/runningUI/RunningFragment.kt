f89d3e564f3af40bdbca8639d4b01237309ceab0
최승준 <csjune99@naver.com>
2024-09-13 13:43:38 +0900

    [#S11P21S004-318] [FE] feat : 러닝 맵에서 지도 부분 presentation layer 기능 구현

---
+++ android/presentation/src/main/java/com/ssafy/presentation/runningUI/RunningFragment.kt
@@ -0,0 +1,87 @@
+package com.ssafy.presentation.runningUI
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.ssafy.presentation.R
+import com.ssafy.presentation.core.BaseFragment
+import com.ssafy.presentation.databinding.FragmentRunningBinding
+
+class RunningFragment : BaseFragment<FragmentRunningBinding>(FragmentRunningBinding::inflate),
+    OnMapReadyCallback {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        initListener()
+        initMapView()
+    }
+
+    private fun initListener() = with(binding) {
+        btnPause.setOnClickListener {
+            btnPlay.showAnimate(true)
+            btnStop.showAnimate(true)
+            btnPause.showAnimate(false)
+        }
+
+        btnPlay.setOnClickListener {
+            btnPause.showAnimate(true)
+            btnPlay.showAnimate(false)
+            btnStop.showAnimate(false)
+        }
+
+        btnStop.setOnClickListener {
+            showSnackStringBar("훈련 종료!")
+        }
+
+        btnMap.setOnClickListener {
+            btnMap.showAnimate(false)
+            btnText.showAnimate(true)
+            runningMap.root.visibility = View.VISIBLE
+            runningText.root.visibility = View.INVISIBLE
+        }
+
+        btnText.setOnClickListener {
+            btnText.showAnimate(false)
+            btnMap.showAnimate(true)
+            runningText.root.visibility = View.VISIBLE
+            runningMap.root.visibility = View.INVISIBLE
+        }
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        val point = LatLng(37.514655, 126.979974)
+        map.addMarker(MarkerOptions().position(point).title("현위치"))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 12f))
+    }
+
+    private fun initMapView() {
+        val mapFragment = SupportMapFragment.newInstance()
+        getParentFragmentManager()
+            .beginTransaction()
+            .add(R.id.map, mapFragment)
+            .commit()
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun ImageButton.showAnimate(isShow: Boolean) {
+        val alphaValue = if (isShow) 1f else 0f
+        animate().apply {
+            duration = 300
+            alpha(alphaValue)
+
+            withStartAction {
+                if (isShow) visibility = View.VISIBLE
+            }
+
+            withEndAction {
+                if (!isShow) visibility = View.INVISIBLE
+            }
+        }
+    }
+}
