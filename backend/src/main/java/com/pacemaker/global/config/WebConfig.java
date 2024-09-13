b20e916c11a859fe9aebc6dfcd87198933252501
김연욱 <ccoma511@naver.com>
2024-09-13 16:00:14 +0900

    [#S11P21S004-269] [BE] chore: CORS 테스트

--- backend/src/main/java/com/pacemaker/global/config/WebConfig.java
+++ backend/src/main/java/com/pacemaker/global/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://j11s004.p.ssafy.io") // 허용할 출처
+			.allowedOrigins("https://j11s004.p.ssafy.io") // 허용할 출처
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 메서드
 			.allowedHeaders("*"); // 허용할 헤더
 	}
