671ecd4638c2212c7d858a011e93ecc4bf76de5d
최승준 <csjune99@naver.com>
2024-09-20 00:23:11 +0900

    [#S11P21S004-342] [FE] feat: data 레이어 구조 단순화 (API Response 제거)

--- android/data/src/main/java/com/ssafy/data/api/UserAPI.kt
+++ android/data/src/main/java/com/ssafy/data/api/UserAPI.kt
@@ -1,6 +1,5 @@
 package com.ssafy.data.api
 
-import com.ssafy.data.response.ApiResponse
 import com.ssafy.domain.dto.Coach
 import com.ssafy.domain.dto.User
 import retrofit2.Response
@@ -14,21 +13,21 @@ import retrofit2.http.Path
 interface UserAPI {
 
     @POST("users")
-    suspend fun signUp(@Body user: User): Response<ApiResponse<Unit>>
+    suspend fun signUp(@Body user: User): Response<Unit>
 
     @PUT("users")
-    suspend fun modify(@Body user: User): Response<ApiResponse<User>>
+    suspend fun modify(@Body user: User): Response<User>
 
     @DELETE("users")
-    suspend fun delete(@Body user: User): Response<ApiResponse<Unit>>
+    suspend fun delete(@Body user: User): Response<Unit>
 
     @GET("users/{uid}")
-    suspend fun getInfo(@Path("uid") uid: String): Response<ApiResponse<User>>
+    suspend fun getInfo(@Path("uid") uid: String): Response<User>
 
     @GET("users/{uid}/coach")
-    suspend fun getCoach(@Path("uid") uid: String): Response<ApiResponse<Coach>>
+    suspend fun getCoach(@Path("uid") uid: String): Response<Coach>
 
     @PUT("users/{uid}/coach")
-    suspend fun setCoach(@Path("uid") uid: String, @Body coach: Coach): Response<ApiResponse<Unit>>
+    suspend fun setCoach(@Path("uid") uid: String, @Body coach: Coach): Response<Unit>
 
 }
--- android/data/src/main/java/com/ssafy/data/source/user/UserDataSource.kt
+++ android/data/src/main/java/com/ssafy/data/source/user/UserDataSource.kt
@@ -1,22 +1,21 @@
 package com.ssafy.data.source.user
 
-import com.ssafy.data.response.ApiResponse
 import com.ssafy.domain.dto.Coach
 import com.ssafy.domain.dto.User
 import retrofit2.Response
 
 interface UserDataSource {
 
-    suspend fun signUp(user: User): Response<ApiResponse<Unit>>
+    suspend fun signUp(user: User): Response<Unit>
 
-    suspend fun modify(user: User): Response<ApiResponse<User>>
+    suspend fun modify(user: User): Response<User>
 
-    suspend fun delete(user: User): Response<ApiResponse<Unit>>
+    suspend fun delete(user: User): Response<Unit>
 
-    suspend fun getInfo(uid: String): Response<ApiResponse<User>>
+    suspend fun getInfo(uid: String): Response<User>
 
-    suspend fun getCoach(uid: String): Response<ApiResponse<Coach>>
+    suspend fun getCoach(uid: String): Response<Coach>
 
-    suspend fun setCoach(uid: String, coach: Coach): Response<ApiResponse<Unit>>
+    suspend fun setCoach(uid: String, coach: Coach): Response<Unit>
 
 }
