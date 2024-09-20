20641a648817fe7b43135553a77405f120a11f9a
박준식 <akfldkskgorn@gmail.com>
2024-09-20 14:02:02 +0900

    [#S11P21S004-287] [FE] feat: wear os theme 적용

--- android/watch/src/main/java/com/ssafy/watch/presentation/theme/Theme.kt
+++ android/watch/src/main/java/com/ssafy/watch/presentation/theme/Theme.kt
@@ -1,17 +1,14 @@
 package com.ssafy.watch.presentation.theme
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.wear.compose.material.MaterialTheme
 
 @Composable
 fun PaceMakerTheme(
     content: @Composable () -> Unit
 ) {
-    /**
-     * Empty theme to customize for your app.
-     * See: https://developer.android.com/jetpack/compose/designsystems/custom
-     */
     MaterialTheme(
+        colorScheme = LightColors,
         content = content
     )
 }
