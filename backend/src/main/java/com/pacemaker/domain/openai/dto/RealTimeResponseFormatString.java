25ff4635c884e575e6632bf93d389124dceacc56
김재훈 <97888471+GarrryKim@users.noreply.github.com>
2024-09-23 22:45:27 +0900

    [#S11P21S004-434] feat: 실시간 러닝 response format 문자열 클래스 추가

---
+++ backend/src/main/java/com/pacemaker/domain/openai/dto/RealTimeResponseFormatString.java
@@ -0,0 +1,31 @@
+package com.pacemaker.domain.openai.dto;
+
+import lombok.Getter;
+
+@Getter
+public class RealTimeResponseFormatString {
+
+	public static String responseFormat = """
+		{
+		  "name": "realtimecoach",
+		  "strict": false,
+		  "schema": {
+		    "type": "object",
+		    "required": [
+		      "textFeedBack",
+		      "textCheer"
+		    ],
+		    "properties": {
+		      "textCheer": {
+		        "type": "string",
+		        "description": "Cheer-up message within 2 sentences in Korean"
+		      },
+		      "textFeedBack": {
+		        "type": "string",
+		        "description": "Feedback message of the about the user's run within 2 sentences in Korean"
+		      }
+		    }
+		  }
+		}
+		""";
+}
