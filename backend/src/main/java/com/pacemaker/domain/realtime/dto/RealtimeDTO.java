2519256e3fcb9d06e18084bf86cfb241dc86667f
김재훈 <97888471+GarrryKim@users.noreply.github.com>
2024-09-23 21:46:49 +0900

    [#S11P21S004-434] feat: 실시간 러닝 Dto 생성

---
+++ backend/src/main/java/com/pacemaker/domain/realtime/dto/RealtimeDTO.java
@@ -0,0 +1,21 @@
+package com.pacemaker.domain.realtime.dto;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public record RealtimeDTO(@NotNull String coachId, @NotNull float totalDistance, @NotNull Integer meanHeartRate,
+						  @NotNull Integer meanPace, @NotNull Integer meanCadence, @NotNull float nowDistance, @NotNull PlanTrain planTrain) {
+
+	@Builder
+	public record PlanTrain(
+		Date trainDate, String paramType, Integer sessionTime, Integer sessionDistance,
+		Integer repetition, Integer trainParam, Integer trainPace, Integer interParam
+	) {
+	}
+}
