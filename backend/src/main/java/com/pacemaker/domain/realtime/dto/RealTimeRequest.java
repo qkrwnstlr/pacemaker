0ac319ec8b51dd25e5ebf182dbd9f9be1abc8252
김재훈 <kgarry@naver.com>
2024-09-24 12:29:04 +0900

    [#S11P21S004-434] [BE] fix: PlanTrain coachId, trainDate 타입 수정

--- backend/src/main/java/com/pacemaker/domain/realtime/dto/RealTimeRequest.java
+++ backend/src/main/java/com/pacemaker/domain/realtime/dto/RealTimeRequest.java
@@ -1,7 +1,5 @@
 package com.pacemaker.domain.realtime.dto;
 
-import java.util.Date;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import jakarta.validation.constraints.NotNull;
@@ -9,13 +7,13 @@ import lombok.Builder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder
-public record RealTimeRequest(@NotNull String coachId, @NotNull Integer meanHeartRate,
+public record RealTimeRequest(@NotNull Long coachId, @NotNull Integer meanHeartRate,
 							  @NotNull Integer meanPace, @NotNull Integer meanCadence, @NotNull float nowDistance,
 							  @NotNull PlanTrain planTrain) {
 
 	@Builder
 	public record PlanTrain(
-		Date trainDate, String paramType, Integer sessionTime, Integer sessionDistance,
+		String trainDate, String paramType, Integer sessionTime, Integer sessionDistance,
 		Integer repetition, Integer trainParam, Integer trainPace, Integer interParam
 	) {
 	}
