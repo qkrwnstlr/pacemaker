c0f392f11959451d578f0c1106838dcee560a072
허유정 <yu.jeong5899@gmail.com>
2024-09-25 21:50:23 +0900

    [#S11P21S004-367] [BE] feat: 플랜 훈련 레포트 생성 기능

--- backend/src/main/java/com/pacemaker/domain/report/service/ReportService.java
+++ backend/src/main/java/com/pacemaker/domain/report/service/ReportService.java
@@ -1,10 +1,31 @@
 package com.pacemaker.domain.report.service;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pacemaker.domain.coach.entity.Coach;
+import com.pacemaker.domain.coach.repository.CoachRepository;
+import com.pacemaker.domain.plan.entity.PlanTrain;
+import com.pacemaker.domain.plan.entity.TrainStatus;
+import com.pacemaker.domain.plan.repository.PlanTrainRepository;
+import com.pacemaker.domain.report.dto.PlanTrainResponse;
 import com.pacemaker.domain.report.dto.ReportFreeRequest;
+import com.pacemaker.domain.report.dto.ReportPlanCreateRequest;
+import com.pacemaker.domain.report.dto.ReportPlanCreateResponse;
+import com.pacemaker.domain.report.dto.SplitData;
+import com.pacemaker.domain.report.dto.TrainEvaluation;
+import com.pacemaker.domain.report.dto.TrainReport;
 import com.pacemaker.domain.report.dto.TrainResult;
+import com.pacemaker.domain.report.entity.Report;
+import com.pacemaker.domain.report.entity.ReportPlanTrain;
+import com.pacemaker.domain.report.entity.ReportType;
+import com.pacemaker.domain.report.repository.ReportPlanTrainRepository;
 import com.pacemaker.domain.report.repository.ReportRepository;
 import com.pacemaker.domain.user.entity.User;
 import com.pacemaker.domain.user.repository.UserRepository;
@@ -18,6 +39,10 @@ public class ReportService {
 
 	private final ReportRepository reportRepository;
 	private final UserRepository userRepository;
+	private final CoachRepository coachRepository;
+	private final PlanTrainRepository planTrainRepository;
+	private final ObjectMapper objectMapper;
+	private final ReportPlanTrainRepository reportPlanTrainRepository;
 
 	@Transactional
 	public void createFree(ReportFreeRequest reportFreeRequest) {
@@ -46,5 +71,108 @@ public class ReportService {
 		//
 		// reportRepository.save(report);
 
+	}
+
+	@Transactional
+	public ReportPlanCreateResponse createReportPlan(ReportPlanCreateRequest reportPlanCreateRequest) throws
+		JsonProcessingException {
+		User user = findUserByUid(reportPlanCreateRequest.uid());
+		PlanTrain planTrain = findPlanTrainByid(reportPlanCreateRequest.planTrainId());
+		Coach coach = findCoachById(reportPlanCreateRequest.coachNumber());
+		planTrain.updatePlanTrainStatus(TrainStatus.DONE);
+
+		// TODO: trainEvaluation을 계산하는 LLM 프롬프팅 필요
+		String stringTrainEvaluation = "{\"paceEvaluation\":75,\"heartRateEvaluation\":70,\"cadenceEvaluation\":85}";
+
+		String heartZone = convertStringHeartZone(reportPlanCreateRequest.trainResult().heartZone());
+		String splitData = convertStringSplitData(reportPlanCreateRequest.trainResult().splitData());
+		String trainMap = convertStringTrainMap(reportPlanCreateRequest.trainResult().trainMap());
+		String coachMessage = convertStringCoachMessage(reportPlanCreateRequest.trainResult().coachMessage());
+
+		Report report = reportRepository.save(Report.builder()
+			.user(user)
+			.trainDate(reportPlanCreateRequest.trainDate())
+			.trainDistance(reportPlanCreateRequest.trainResult().trainDistance())
+			.trainTime(reportPlanCreateRequest.trainResult().trainTime())
+			.heartRate(reportPlanCreateRequest.trainResult().heartRate())
+			.pace(reportPlanCreateRequest.trainResult().pace())
+			.cadence(reportPlanCreateRequest.trainResult().cadence())
+			.kcal(reportPlanCreateRequest.trainResult().kcal())
+			.heartZone(heartZone)
+			.splitData(splitData)
+			.trainMap(trainMap)
+			.reportType(ReportType.PLAN)
+			.trainEvaluation(stringTrainEvaluation)
+			.build());
+
+		reportPlanTrainRepository.save(ReportPlanTrain.builder()
+			.report(report)
+			.planTrain(planTrain)
+			.coach(coach)
+			.coachMessage(coachMessage)
+			.build());
+
+		Integer planTrainIndex = findIndexByPlanTrainId(reportPlanCreateRequest.planTrainId());
+		PlanTrainResponse planTrainResponse = PlanTrainResponse.of(planTrainIndex, planTrain);
+		TrainResult trainResult = TrainResult.of(report, reportPlanCreateRequest.trainResult().coachMessage());
+		TrainEvaluation trainEvaluation = objectMapper.readValue(stringTrainEvaluation, TrainEvaluation.class);
+		TrainReport trainReport = TrainReport.builder()
+			.trainDate(calculateTrainDuration(reportPlanCreateRequest.trainDate(),
+				reportPlanCreateRequest.trainResult().trainTime()))
+			.trainResult(trainResult)
+			.trainEvaluation(trainEvaluation)
+			.build();
+
+		return ReportPlanCreateResponse.builder()
+			.planTrainResponse(planTrainResponse)
+			.trainReport(trainReport)
+			.build();
+	}
+
+	private User findUserByUid(String uid) {
+		return userRepository.findByUid(uid)
+			.orElseThrow(() -> new NotFoundException("해당 사용자를 찾을 수 없습니다."));
+	}
+
+	private PlanTrain findPlanTrainByid(Long id) {
+		return planTrainRepository.findPlanTrainById(id)
+			.orElseThrow(() -> new NotFoundException("해당 훈련을 찾을 수 없습니다."));
+	}
+
+	private Coach findCoachById(Long coachId) {
+		return coachRepository.findById(coachId)
+			.orElseThrow(() -> new NotFoundException("존재하지 않는 코치입니다."));
+	}
+
+	private Integer findIndexByPlanTrainId(Long planTrainId) {
+		Integer index = planTrainRepository.findIndexByPlanTrainId(planTrainId);
+
+		if (index == -1) {
+			throw new NotFoundException("해당 훈련을 찾을 수 없습니다.");
+		}
+
+		return index;
+	}
+
+	private String convertStringHeartZone(List<Integer> heartZone) throws JsonProcessingException {
+		return objectMapper.writeValueAsString(heartZone);
+	}
+
+	private String convertStringSplitData(SplitData splitData) throws JsonProcessingException {
+		return objectMapper.writeValueAsString(splitData);
+	}
+
+	private String convertStringTrainMap(List<List<Double>> trainMap) throws JsonProcessingException {
+		return objectMapper.writeValueAsString(trainMap);
+	}
+
+	private String convertStringCoachMessage(List<String> coachMessage) throws JsonProcessingException {
+		return objectMapper.writeValueAsString(coachMessage);
+	}
+
+	private List<LocalTime> calculateTrainDuration(LocalDateTime endDateTiem, Integer trainTime) {
+		LocalTime endTime = endDateTiem.toLocalTime();
+		LocalTime startTime = endTime.minusSeconds(trainTime);
+		return List.of(startTime, endTime);
 	}
 }
