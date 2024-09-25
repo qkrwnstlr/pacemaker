92d61686116897ee5f5606bb0b3d0047fd0913d4
최승준 <csjune99@naver.com>
2024-09-25 20:19:49 +0900

    [#S11P21S004-389] [FE] feat: 플랜 유무 확인 및 그에 맞는 로직 처리

--- android/presentation/src/main/java/com/ssafy/presentation/homeUI/HomeViewModel.kt
+++ android/presentation/src/main/java/com/ssafy/presentation/homeUI/HomeViewModel.kt
@@ -2,10 +2,12 @@ package com.ssafy.presentation.homeUI
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ssafy.domain.repository.DataStoreRepository
+import com.ssafy.domain.usecase.plan.GetPlanInfoUseCase
 import com.ssafy.presentation.core.exercise.ExerciseRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import com.ssafy.domain.repository.DataStoreRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -16,7 +18,8 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val dataStoreRepository: DataStoreRepository,
-    private val exerciseRepository: ExerciseRepository
+    private val exerciseRepository: ExerciseRepository,
+    private val getPlanInfoUseCase: GetPlanInfoUseCase
 ) : ViewModel() {
     private val _trainingState: MutableStateFlow<Int> = MutableStateFlow(4)
     val trainingState: StateFlow<Int> = _trainingState.asStateFlow()
@@ -27,10 +30,17 @@ class HomeViewModel @Inject constructor(
     fun profileUrlFlow(): Flow<String> {
         return dataStoreRepository.getImgUrl()
     }
+
     fun startExercise() {
         viewModelScope.launch {
             delay(3_000)
             exerciseRepository.startExercise()
         }
+    }
+
+    fun getPlanInfo(uid: String) = viewModelScope.launch(Dispatchers.IO) {
+        runCatching { getPlanInfoUseCase(uid) }
+            .onSuccess { _trainingState.emit(3) }
+            .onFailure { _trainingState.emit(4) }
     }
 }
