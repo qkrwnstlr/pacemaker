92837cffdcb336721c31303bcec878196be475d5
허유정 <yu.jeong5899@gmail.com>
2024-09-26 17:19:29 +0900

    [#S11P21S004-370] [BE] feat: 플랜 레포트 조회

--- backend/src/main/java/com/pacemaker/domain/report/controller/ReportController.java
+++ backend/src/main/java/com/pacemaker/domain/report/controller/ReportController.java
@@ -2,6 +2,8 @@ package com.pacemaker.domain.report.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.pacemaker.domain.report.dto.ReportFreeRequest;
 import com.pacemaker.domain.report.dto.ReportPlanCreateRequest;
-import com.pacemaker.domain.report.dto.ReportPlanCreateResponse;
+import com.pacemaker.domain.report.dto.ReportPlanResponse;
 import com.pacemaker.domain.report.service.ReportService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -47,8 +49,24 @@ public class ReportController {
 		@ApiResponse(responseCode = "404", description = "코치정보 조회 실패"),
 		@ApiResponse(responseCode = "404", description = "훈련정보 조회 실패")
 	})
-	public ResponseEntity<ReportPlanCreateResponse> createPlanReport(
+	public ResponseEntity<ReportPlanResponse> createReportPlan(
 		@RequestBody ReportPlanCreateRequest reportPlanCreateRequest) throws JsonProcessingException {
 		return ResponseEntity.status(HttpStatus.CREATED).body(reportService.createReportPlan(reportPlanCreateRequest));
+	}
+
+	@GetMapping("/{id}/plan/user/{uid}")
+	@Operation(summary = "레포트 조회 - 플랜 훈련")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "플랜 훈련 레포트 조회 성공"),
+		@ApiResponse(responseCode = "400", description = "회원정보 불일치"),
+		@ApiResponse(responseCode = "404", description = "레포트정보 조회 실패"),
+		@ApiResponse(responseCode = "404", description = "레포트훈련정보 조회 실패"),
+		@ApiResponse(responseCode = "404", description = "회원정보 조회 실패"),
+		@ApiResponse(responseCode = "404", description = "코치정보 조회 실패"),
+		@ApiResponse(responseCode = "404", description = "훈련정보 조회 실패")
+	})
+	public ResponseEntity<ReportPlanResponse> findReportPlan(@PathVariable Long id, @PathVariable String uid) throws
+		JsonProcessingException {
+		return ResponseEntity.status(HttpStatus.OK).body(reportService.findReportPlan(id, uid));
 	}
 }
