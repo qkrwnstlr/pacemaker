3f9371bfa73cb178b11f5bc5970ab2ac47579d73
최승준 <csjune99@naver.com>
2024-09-26 20:28:08 +0900

    [#S11P21S004-449] [FE] feat: 실시간 코칭 response 변환 (문자열 -> TTS)

--- android/domain/src/main/java/com/ssafy/domain/repository/TrainRepository.kt
+++ android/domain/src/main/java/com/ssafy/domain/repository/TrainRepository.kt
@@ -1,9 +1,9 @@
 package com.ssafy.domain.repository
 
 import com.ssafy.domain.dto.train.CoachingRequest
-import com.ssafy.domain.dto.train.CoachingResponse
 import com.ssafy.domain.response.ResponseResult
+import java.io.File
 
 interface TrainRepository {
-    suspend fun getCoaching(dto: CoachingRequest): ResponseResult<CoachingResponse>
+    suspend fun getCoaching(coachingRequest: CoachingRequest): ResponseResult<File>
 }
