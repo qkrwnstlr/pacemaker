2f1d348c6ff4566e3b1e8d8b4fc1244c70545ca5
허유정 <yu.jeong5899@gmail.com>
2024-09-27 16:17:41 +0900

    [#S11P21S004-455] [BE] feat: 훈련 평가 DTO 생성

--- backend/src/main/java/com/pacemaker/domain/report/dto/SplitData.java
+++ backend/src/main/java/com/pacemaker/domain/report/dto/SplitData.java
@@ -4,5 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record SplitData(@JsonProperty("gradeDistance") Integer gradeDistance,
 						@JsonProperty("gradePace") Integer gradePace,
-						@JsonProperty("gradeHeartRate") Integer gradeHeartRate) {
+						@JsonProperty("gradeHeartRate") Integer gradeHeartRate,
+						@JsonProperty("gradeCadence") Integer gradeCadence) {
 }
