b21554fc82c5a4674d58c1db71a96b1ffb3682b9
김재훈 <kgarry@naver.com>
2024-09-27 15:47:09 +0900

    [#S11P21S004-440] [BE] feat: 실시간 피드백 TTS 변경 서비스 로직 추가 및 LLM 응답 null 해결

--- backend/src/main/java/com/pacemaker/domain/realtime/controller/RealTimeController.java
+++ backend/src/main/java/com/pacemaker/domain/realtime/controller/RealTimeController.java
@@ -3,6 +3,7 @@ package com.pacemaker.domain.realtime.controller;
 import java.time.Duration;
 import java.time.Instant;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.pacemaker.domain.openai.service.OpenAiService;
 import com.pacemaker.domain.realtime.dto.RealTimeRequest;
+import com.pacemaker.domain.realtime.service.RealTimeService;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
@@ -21,17 +23,27 @@ import reactor.core.publisher.Mono;
 public class RealTimeController {
 
 	private final OpenAiService openAiService;
+	private final RealTimeService realTimeService;
 
 	@PostMapping
 	public Mono<ResponseEntity<?>> createRealTimeFeedback(@RequestBody RealTimeRequest realTimeRequest) {
 		Instant start = Instant.now();
 
 		return openAiService.createRealTimeChatCompletions(realTimeRequest)
-			.map(response -> {
+			.flatMap(response -> {
 				long timeElapsed = Duration.between(start, Instant.now()).toMillis();
-				System.out.println("Request processing time: " + timeElapsed + " milliseconds"); // 나중에 log로 바꾸기
+				System.out.println("GPT processing time: " + timeElapsed + " milliseconds"); // 나중에 log로 바꾸기
 
-				return ResponseEntity.ok(response);
+				return realTimeService.createRealTimeTts(response, realTimeRequest.coachIndex());
+			})
+			.map(wavFile -> {
+				long timeElapsed = Duration.between(start, Instant.now()).toMillis();
+				System.out.println("Total processing time: " + timeElapsed + " milliseconds");
+
+				return ResponseEntity.ok()
+					.header(HttpHeaders.CONTENT_TYPE, "audio/wav")
+					.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"feedback.wav\"")
+					.body(wavFile);
 			});
 	};
 }
