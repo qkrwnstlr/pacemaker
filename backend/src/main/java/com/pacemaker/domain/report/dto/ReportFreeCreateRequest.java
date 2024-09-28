531785ae79e1382de430639f08385d1e861c4ed3
허유정 <yu.jeong5899@gmail.com>
2024-09-28 17:34:43 +0900

    [#S11P21S004-457] [BE] feat: git 충돌 해결

---
+++ backend/src/main/java/com/pacemaker/domain/report/dto/ReportFreeCreateRequest.java
@@ -0,0 +1,9 @@
+package com.pacemaker.domain.report.dto;
+
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotNull;
+
+public record ReportFreeCreateRequest(@NotNull String uid, @NotNull LocalDateTime trainDate,
+									  @NotNull TrainResult trainResult) {
+};
