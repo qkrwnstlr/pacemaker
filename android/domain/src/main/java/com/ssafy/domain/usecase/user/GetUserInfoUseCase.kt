cc892be2fd2640d2345a49569b2c4a675d87b855
최승준 <csjune99@naver.com>
2024-09-29 21:56:15 +0900

    [#S11P21S004-462] [FE] feat: 유저 정보가 있는 경우 data store 최신화 + uid 저장

--- android/domain/src/main/java/com/ssafy/domain/usecase/user/GetUserInfoUseCase.kt
+++ android/domain/src/main/java/com/ssafy/domain/usecase/user/GetUserInfoUseCase.kt
@@ -1,14 +1,24 @@
 package com.ssafy.domain.usecase.user
 
 import com.ssafy.domain.dto.User
+import com.ssafy.domain.repository.DataStoreRepository
 import com.ssafy.domain.repository.UserRepository
 import com.ssafy.domain.response.ResponseResult
 import javax.inject.Inject
 
-class GetUserInfoUseCase @Inject constructor(private val userRepository: UserRepository) {
+class GetUserInfoUseCase @Inject constructor(
+    private val userRepository: UserRepository,
+    private val dataStoreRepository: DataStoreRepository
+) {
+
     suspend operator fun invoke(uid: String): User {
         val response = userRepository.getInfo(uid)
         if (response is ResponseResult.Error) throw RuntimeException(response.message)
-        return response.data ?: throw RuntimeException()
+
+        val user = response.data ?: throw RuntimeException()
+        val newUser = user.copy(uid = uid)
+        dataStoreRepository.saveUser(newUser)
+        return response.data
     }
+
 }
