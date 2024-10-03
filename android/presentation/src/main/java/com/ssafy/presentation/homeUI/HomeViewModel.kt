1ae92bcddbceceadd6bb17337e324f94e28af087
최승준 <csjune99@naver.com>
2024-10-03 23:00:08 +0900

    [#S11P21S004-525] [FE] feat: 홈 화면 로직 변경 + 탑 시트의 최대 위치 제한

--- android/presentation/src/main/java/com/ssafy/presentation/homeUI/HomeViewModel.kt
+++ android/presentation/src/main/java/com/ssafy/presentation/homeUI/HomeViewModel.kt
@@ -8,7 +8,7 @@ import com.ssafy.domain.dto.schedule.ContentListDto
 import com.ssafy.domain.repository.DataStoreRepository
 import com.ssafy.domain.usecase.plan.GetPlanInfoUseCase
 import com.ssafy.domain.usecase.reports.GetReportUseCase
-import com.ssafy.domain.usecase.user.GetTermCalendarDotUseCase
+import com.ssafy.domain.usecase.user.GetCalendarInfoUseCase
 import com.ssafy.presentation.core.exercise.ExerciseRepository
 import com.ssafy.presentation.utils.toCoachIndex
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -28,11 +28,9 @@ class HomeViewModel @Inject constructor(
     private val dataStoreRepository: DataStoreRepository,
     private val exerciseRepository: ExerciseRepository,
     private val getPlanInfoUseCase: GetPlanInfoUseCase,
-    private val getTermCalendarDotUseCase: GetTermCalendarDotUseCase,
+    private val getCalendarInfoUseCase: GetCalendarInfoUseCase,
     private val getReportUseCase: GetReportUseCase
 ) : ViewModel() {
-    private val _trainingState: MutableStateFlow<Int> = MutableStateFlow(3)
-    val trainingState: StateFlow<Int> = _trainingState.asStateFlow()
 
     private val _coachState: MutableStateFlow<Int> = MutableStateFlow(1)
     val coachState: StateFlow<Int> = _coachState.asStateFlow()
@@ -50,38 +48,24 @@ class HomeViewModel @Inject constructor(
         dataStoreRepository.setLocation(latitude, longitude)
     }
 
-    suspend fun getLatitude(): Double =
-        dataStoreRepository.getLatitude().firstOrNull() ?: 0.0
+    suspend fun getLatitude(): Double = dataStoreRepository.getLatitude().firstOrNull() ?: 0.0
 
-
-    suspend fun getLongitude(): Double =
-        dataStoreRepository.getLongitude().firstOrNull() ?: 0.0
-
+    suspend fun getLongitude(): Double = dataStoreRepository.getLongitude().firstOrNull() ?: 0.0
 
     fun getCoach() = viewModelScope.launch(Dispatchers.IO) {
-        runCatching {
-            dataStoreRepository.getUser().coachNumber.toCoachIndex()
-        }
+        runCatching { dataStoreRepository.getUser().coachNumber.toCoachIndex() }
             .onSuccess { _coachState.emit(it) }
     }
 
-    fun setTermMonthHasTrain(
-        uid: String,
-        startYear: Int,
-        startMonth: Int,
-        currentYear: Int,
-        currentMonth: Int,
-        endYear: Int,
-        endMonth: Int
-    ) {
-        viewModelScope.launch {
-            runCatching {
-                getTermCalendarDotUseCase.invoke(
-                    uid, startYear, startMonth, currentYear, currentMonth, endYear, endMonth
-                )
-            }.onSuccess {
-                _dateList.emit(it)
-            }
+    fun getTrain() = viewModelScope.launch(Dispatchers.IO) {
+        val date = LocalDate.now()
+        runCatching {
+            getCalendarInfoUseCase(date)
+        }.onSuccess {
+            _dateList.emit(it)
+            setDateTrainInfo(date)
+        }.onFailure {
+            it.printStackTrace()
         }
     }
 
@@ -95,44 +79,68 @@ class HomeViewModel @Inject constructor(
         }
     }
 
-    fun getPlanInfo() = viewModelScope.launch(Dispatchers.IO) {
-        runCatching { getPlanInfoUseCase() }.onSuccess {
-            _report.emit(Report(PlanTrain()))
-            _trainingState.emit(3)
-        }
-            .onFailure {
-                _report.emit(Report(null))
-                _trainingState.emit(4)
-            }
-    }
-
     fun setDate(localDate: LocalDate) = viewModelScope.launch(Dispatchers.IO) {
         _selectDate.update { localDate }
-        if (dateList.value.containsKey(localDate.toString())) {
-            val reportList = dateList.value[localDate.toString()]
-            if (reportList != null) {
-                for (i in reportList.indices) {
-                    if (reportList[i].type == "BEFORE_PLAN_TRAIN") {
-                        _report.emit(
-                            getReportUseCase.invoke(
-                                reportList[i],
-                                dataStoreRepository.getUser().uid
-                            )
-                        )
-                        _trainingState.emit(1)
-                    } else if (i == reportList.size - 1) {
-                        _report.emit(
-                            getReportUseCase.invoke(
-                                reportList[i],
-                                dataStoreRepository.getUser().uid
-                            )
-                        )
-                        _trainingState.emit(2)
-                    }
-                }
+        setDateTrainInfo(localDate)
+    }
+
+    private suspend fun setDateTrainInfo(localDate: LocalDate) {
+        val reportList = dateList.value[localDate.toString()] ?: emptyList()
+
+        if (reportList.isEmpty()) getPlanInfo(localDate)
+        else getReportInfo(reportList, localDate)
+    }
+
+    private suspend fun getPlanInfo(localDate: LocalDate) = runCatching {
+        getPlanInfoUseCase()
+    }.onSuccess {
+        val newReport = Report(
+            planTrain = PlanTrain(),
+            trainingState = TRAIN_REST_MESSAGE,
+            date = localDate
+        )
+        _report.emit(newReport)
+    }.onFailure {
+        val newReport = Report(
+            trainingState = CREATE_PLAN,
+            date = localDate
+        )
+        _report.emit(newReport)
+    }
+
+    private suspend fun getReportInfo(reportList: List<ContentListDto>, localDate: LocalDate) =
+        runCatching {
+            val reportGroup = reportList.groupBy { it.type }
+            val content = when {
+                !reportGroup[PLAN_REPORT].isNullOrEmpty() -> reportGroup[PLAN_REPORT]?.lastOrNull()
+                !reportGroup[BEFORE_PLAN_TRAIN].isNullOrEmpty() -> reportGroup[BEFORE_PLAN_TRAIN]?.lastOrNull()
+                !reportGroup[FREE_REPORT].isNullOrEmpty() -> reportGroup[FREE_REPORT]?.lastOrNull()
+                else -> null
             }
-        } else {
-            getPlanInfo()
+
+            content?.let { getReportUseCase(it) }
+        }.onSuccess { reportInfo ->
+            if (reportInfo == null) return@onSuccess
+
+            val newReport = if (reportInfo.trainReport == null) {
+                reportInfo.copy(trainingState = TRAIN_INFO, date = localDate)
+            } else {
+                reportInfo.copy(trainingState = TRAIN_RESULT, date = localDate)
+            }
+
+            _report.emit(newReport)
+        }.onFailure {
+            it.printStackTrace()
         }
+
+    companion object {
+        const val FREE_REPORT = "FREE_REPORT"
+        const val PLAN_REPORT = "PLAN_REPORT"
+        const val BEFORE_PLAN_TRAIN = "BEFORE_PLAN_TRAIN"
+
+        const val TRAIN_INFO = 1
+        const val TRAIN_RESULT = 2
+        const val TRAIN_REST_MESSAGE = 3
+        const val CREATE_PLAN = 4
     }
 }
