888cc9e76ed87c3bb0b524e95739ccf936eece5b
최승준 <csjune99@naver.com>
2024-10-06 20:43:18 +0900

    [#S11P21S004-532] [FE] fix: 유저 정보 수정시 uid 가 제거되는 버그 해결

--- android/domain/src/main/java/com/ssafy/domain/usecase/user/ModifyUserUseCase.kt
+++ android/domain/src/main/java/com/ssafy/domain/usecase/user/ModifyUserUseCase.kt
@@ -1,11 +1,30 @@
 package com.ssafy.domain.usecase.user
 
 import com.ssafy.domain.dto.User
+import com.ssafy.domain.repository.DataStoreRepository
 import com.ssafy.domain.repository.UserRepository
 import com.ssafy.domain.response.ResponseResult
 import javax.inject.Inject
 
-class ModifyUserUseCase @Inject constructor(private val userRepository: UserRepository) {
-    suspend operator fun invoke(uid: String, user: User): ResponseResult<User> =
-        userRepository.modify(uid, user)
+class ModifyUserUseCase @Inject constructor(
+    private val userRepository: UserRepository,
+    private val dataStoreRepository: DataStoreRepository
+) {
+    suspend operator fun invoke(uid: String, user: User): User {
+        val response = userRepository.modify(uid, user)
+        if (response is ResponseResult.Error) throw RuntimeException(response.message)
+
+        val responseUser = response.data ?: throw RuntimeException()
+        val newUser = dataStoreRepository.getUser().copy(
+            uid = uid,
+            name = responseUser.name,
+            age = responseUser.age,
+            height = responseUser.height,
+            weight = responseUser.weight,
+            gender = responseUser.gender
+        )
+
+        dataStoreRepository.saveUser(newUser)
+        return newUser
+    }
 }
