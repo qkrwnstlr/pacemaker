4697fc494e4638307200f51496f2663e91572c1f
박준식 <akfldkskgorn@gmail.com>
2024-10-06 13:20:14 +0900

    [FE] release watch 1.0.6 version

--- android/watch/build.gradle.kts
+++ android/watch/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.5"
+        versionName = "1.0.6"
         vectorDrawables {
             useSupportLibrary = true
         }
