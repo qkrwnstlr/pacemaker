7e0f5db77ddd46472ecbe59d731a3ddc54dd9485
최승준 <csjune99@naver.com>
2024-10-06 13:00:47 +0900

    [#S11P21S004-531] [FE] fix: 처음 회원 가입할때 uid가 사라지는 버그 해결

--- android/domain/src/main/java/com/ssafy/domain/usecase/user/SignUpUseCase.kt
+++ android/domain/src/main/java/com/ssafy/domain/usecase/user/SignUpUseCase.kt
@@ -2,13 +2,23 @@ package com.ssafy.domain.usecase.user
 
 import com.ssafy.domain.dto.LoginRequestBody
 import com.ssafy.domain.dto.LoginResponseBody
+import com.ssafy.domain.repository.DataStoreRepository
 import com.ssafy.domain.repository.UserRepository
 import com.ssafy.domain.response.ResponseResult
 import javax.inject.Inject
 
-class SignUpUseCase @Inject constructor(private val userRepository: UserRepository) {
-    suspend operator fun invoke(uid: String, name: String?): ResponseResult<LoginResponseBody> {
-        return userRepository.signUp(uid, LoginRequestBody(name ?: RUNNER))
+class SignUpUseCase @Inject constructor(
+    private val userRepository: UserRepository,
+    private val dataStoreRepository: DataStoreRepository
+) {
+    suspend operator fun invoke(uid: String, name: String?): LoginResponseBody {
+        val response = userRepository.signUp(uid, LoginRequestBody(name ?: RUNNER))
+        if (response is ResponseResult.Error) throw RuntimeException(response.message)
+
+        val user = response.data?.userInfoResponse ?: throw RuntimeException()
+        val newUser = user.copy(uid = uid)
+        dataStoreRepository.saveUser(newUser)
+        return response.data
     }
 
     companion object {
